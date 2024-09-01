b0f11b8a3c29055b75a73cf6166539bed3881c6e
JyojiTenguam <j.y.oji@hotmail.com>
2024-09-01 03:42:33 -0300

    fix: Mudando arquivo index.ts para outra pasta

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Race from './Races/Race';
-import Dwarf from './Races/Dwarf';
-import Elf from './Races/Elf';
-import Halfling from './Races/Halfling';
-import Orc from './Races/Orc';
-
-export { Dwarf, Elf, Halfling, Orc };
-export default Race;
