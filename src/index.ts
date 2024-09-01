c237d4b2ad82b97faea306f56662f9fd905b1016
JyojiTenguam <j.y.oji@hotmail.com>
2024-09-01 03:11:10 -0300

    Resolvendo requisito 02 de Trybers and Dragons

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import Race from './Races/Race';
+import Dwarf from './Races/Dwarf';
+import Elf from './Races/Elf';
+import Halfling from './Races/Halfling';
+import Orc from './Races/Orc';
 
+export { Dwarf, Elf, Halfling, Orc };
 export default Race;
