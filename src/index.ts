f7a8535858d2b0dbaa2268f428d3ac73911e4c60
JyojiTenguam <j.y.oji@hotmail.com>
2024-09-02 20:02:04 -0300

    fix: Retirando console.log do código

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,9 @@ import Monster from './Monster';
 import Dragon from './Dragon';
 import Battle, { PVE, PVP } from './Battle';
 
-console.log(Character);
 const player1 = new Character('Player 1');
 const player2 = new Character('Player 2');
 const player3 = new Character('Player 3');
-
-console.log(player1);
-console.log(player2);
-console.log(player3);
 
 player1.levelUp();
 player1.levelUp();
@@ -19,14 +14,9 @@ player1.levelUp();
 const monster1 = new Monster();
 const monster2 = new Dragon();
 
-console.log(monster1);
-console.log(monster2);
-
 const pvp = new PVP(player2, player3);
-console.log(pvp);
 
 const pve = new PVE(player1, [monster1, monster2]);
-console.log(pve);
 
 function runBattles(battles: Battle[]): void {
   battles.forEach((battle) => battle.fight());
